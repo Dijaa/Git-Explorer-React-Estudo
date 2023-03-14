8fe79657d5c3b073d47b7008fe12135d698ab237
Javan <javan_dhein@outlook.com>
2023-03-13 21:13:55 -0300

    FEAT: fetch on API

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import { render } from "react-dom";
-import { App } from "./App"
-
-render(<App />, document.getElementById('root'))
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
