dcd9b8be513c9c345a3f1688d255982fa821b7f3
Javan <javan_dhein@outlook.com>
2023-03-14 21:24:57 -0300

    SETINGS

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import { App } from "./App";
-import { render } from "react-dom";
-// import { createRoot } from "react-dom/client";
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-// root.render(<App />);
-render(<App />, document.getElementById("root"));
